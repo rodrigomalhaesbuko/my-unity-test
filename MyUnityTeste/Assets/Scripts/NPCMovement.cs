cb2113d5f3d7ed24a7d7f08ce52c6183c5ff7707
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2020-05-27 23:30:56 -0300

    First steps

---
+++ MyUnityTeste/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    public float npcSpeed;
+    public GameObject npc;
+    public float acceleration;
+    float vel = 0; 
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (npc == null)
+        {
+            Debug.Log("Error: no Player found");
+            return;
+        }
+
+        Vector3 pos = npc.transform.position;
+        vel +=  npcSpeed * acceleration;
+        pos.x += vel;
+
+        npc.transform.position = pos;
+
+    }
+}
---
+++ MyUnityTeste/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float mySpeed;
+    private GameObject player;
+
+    void Start()
+    {
+        // mySpeed = ReturnMySpeed();
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+
+    void FixedUpdate()
+    {
+        if (player == null)
+        {
+            Debug.Log("Error: no Player found");
+            return;
+        }
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis ("Vertical");
+
+
+        Vector3 pos = player.transform.position;
+        pos.x += mySpeed * moveHorizontal;
+        pos.y +=  mySpeed * moveVertical;
+
+        player.transform.position = pos;
+
+    }
+
+    // int ReturnMySpeed(){
+    //     return 30;
+    // }
+}
