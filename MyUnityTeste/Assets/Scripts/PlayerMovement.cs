00083ca60b33645ce87fac8b9a64128507abdca7
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2020-05-30 23:35:06 -0300

    Basic GameObjects and Component

--- MyUnityTeste/Assets/Scripts/PlayerMovement.cs
+++ MyUnityTeste/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,12 @@ public class PlayerMovement : MonoBehaviour
 
     public float mySpeed;
     private GameObject player;
+    public Vector3 initialPos;
 
     void Start()
     {
-        // mySpeed = ReturnMySpeed();
         player = GameObject.FindGameObjectWithTag("player");
+        initialPos = player.transform.position;
     }
 
     void FixedUpdate()
@@ -26,12 +27,30 @@ public class PlayerMovement : MonoBehaviour
         //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis ("Vertical");
 
-
         Vector3 pos = player.transform.position;
-        pos.x += mySpeed * moveHorizontal;
-        pos.y +=  mySpeed * moveVertical;
+        Vector3 dir = new Vector3(moveHorizontal,moveVertical).normalized;
 
-        player.transform.position = pos;
+        Vector3 targetPos = player.transform.position + dir * mySpeed;
+
+        RaycastHit2D hit = Physics2D.Raycast(pos + (float)0.04*dir,dir,mySpeed);
+
+        if(hit.collider == null ){
+            player.transform.position = targetPos;
+
+
+        }else {
+            if(hit.collider.tag == "Finish"){
+                player.GetComponent<SpriteRenderer>().color = Color.yellow;
+                player.transform.localScale = player.transform.localScale * (float) 1;
+            }else if(hit.collider.tag == "WallPuto") {
+                player.transform.position = initialPos;
+            }else if(hit.collider.tag == "Wall") {
+                // faz nada 
+            }
+        }
+        
+
+        
 
     }
 
